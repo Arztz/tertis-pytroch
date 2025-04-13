62c2e596a679919eb7e03c9cf2dfec16067d2b33
arztz <you@example.com>
2025-04-13 23:20:47 +0700

    finish tertis

--- code/main.py
+++ code/main.py
@@ -3,6 +3,8 @@ from sys import exit
 from game import Game
 from score import Score
 from preview import Preview
+from random import choice
+from os.path import join
 class Main:
     def __init__(self):
         #general
@@ -11,10 +13,29 @@ class Main:
         self.clock = pygame.time.Clock()
         pygame.display.set_caption('Tertis')
 
+        #shapes
+        self.next_shapes = [choice(list(TETROMINOS.keys())) for shape in range(3)]
+
         #component
-        self.game = Game()
+        self.game = Game(self.get_next_shape,self.update_score)
         self.score = Score()
         self.preview = Preview()
+
+        #audio
+        self.music = pygame.mixer.Sound(join('sound','music.wav'))
+        self.music.set_volume(0.05)
+        self.music.play(-1)
+
+    def update_score(self,lines,score,level):
+        self.score.lines = lines
+        self.score.score = score
+        self.score.level = level
+
+    def get_next_shape(self):
+        next_shape = self.next_shapes.pop(0)
+        self.next_shapes.append(choice(list(TETROMINOS.keys())))
+        return next_shape
+    
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -26,7 +47,7 @@ class Main:
             self.display_surface.fill(GRAY)
             self.game.run()
             self.score.run()
-            self.preview.run()
+            self.preview.run(self.next_shapes)
             #updating the game
             pygame.display.update()
             self.clock.tick()
